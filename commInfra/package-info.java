f48fa5f84664e96ed22a14da334bb3e0264a1377
Daniel Magueta <daniel.magueta@hotmail.com>
2021-06-28 11:17:14 +0100

    commInfra completed

---
+++ commInfra/package-info.java
@@ -0,0 +1,8 @@
+/**
+ *  Common infrastructure for the AirLift Problem.
+ *
+ *    Implementation of a client-server model of type 2 (server replication).
+ *    Communication is based on Java RMI.
+ */
+
+package commInfra;
